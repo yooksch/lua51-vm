6f628d51c22c453263c6ee96d94ea8ac0bd106e2
yooksch <yooksch@pm.me>
2025-03-18 00:23:20 +0100

    Fix vararg, fix forloop, add instruction notations

--- src/vm.rs
+++ src/vm.rs
@@ -1,7 +1,9 @@
-use std::{cell::RefCell, collections::BTreeMap, ops::Sub, rc::Rc, sync::{Arc, Mutex}};
+use std::{cell::RefCell, collections::BTreeMap, ops::Sub, rc::Rc};
 
-use crate::{bytecode::{LuaPrototype, OpCode, FIELDS_PER_FLUSH}, libs, lua_function, lua_string, lua_table, types::{LuaError, LuaFunction, LuaResult, LuaValue}};
+use crate::{bytecode::{LuaPrototype, OpCode, FIELDS_PER_FLUSH}, libs, lua_function, types::{LuaError, LuaResult, LuaValue}};
 
+// Simplify getting indexing the constants list or stack
+// B and C can be above 255 (max stack size) to indicate that they are referencing a constant
 macro_rules! get_rk {
     ($idx:expr, $constants:ident, $stack:ident) => {
         if $idx >= 256 {
@@ -27,6 +29,7 @@ impl VirtualMachine {
     }
 
     pub fn load_std_libraries(&mut self) {
+        // Merge the two maps, overwrite any pre-existing members
         let insert = |t: BTreeMap<Rc<RefCell<LuaValue>>, Rc<RefCell<LuaValue>>>| {
             for (k, v) in t.iter() {
                 self.environment.borrow_mut().insert(k.clone(), v.clone());
@@ -41,47 +44,68 @@ impl VirtualMachine {
             Some(v) => v,
             None => Vec::new()
         };
-        let vararg = match vararg {
+        let mut vararg = match vararg {
             Some(v) => v,
             None => Vec::new()
         };
 
         let mut pc = 0i64;
-        let mut stack: Vec<Rc<RefCell<LuaValue>>> = vec![Rc::new(RefCell::new(LuaValue::Nil)); function.max_stack_size.into()];
+        let mut stack: Vec<Rc<RefCell<LuaValue>>> = vec![Rc::new(RefCell::new(LuaValue::Nil)); 255];
         let mut stack_top = 0usize;
 
         // push args onto the stack
         if let Some(args) = args {
-            for i in 0..args.len() {
+            for i in 0..function.param_count as usize {
                 stack[i] = args[i].clone();
+            }
+
+            // push excess args into the vararg vector
+            for i in function.param_count as usize..args.len() {
+                vararg.push(args[i].clone());
             }
         }
 
         let instructions = function.instructions;
         let constants = function.constants;
+        
+        /* 
+        Instruction notation:
+        S = stack
+        K = constants
+        SK = stack/constants, see get_rk
+        PC = program counter
+        E = environment
+        UV = upvalue
+        */
         while pc < instructions.len() as i64 {
             let inst = &instructions[pc as usize];
             match inst.code {
+                // S[A] = S[B]
                 OpCode::Move => {
                     stack[inst.A] = stack[inst.B].clone();
                 },
+                // S[A]..S[B] = nil
                 OpCode::LoadNil => {
                     for i in inst.A..inst.B {
                         stack[i] = LuaValue::Nil.into();
                     }
                 },
+                // S[A] = K[Bx]
                 OpCode::LoadK => {
                     stack[inst.A] = match constants.get(inst.Bx) {
                         Some(k) => k.clone(),
                         None => return LuaResult::Err(LuaError::ConstantNotFound(inst.Bx))
                     };
                 },
+                // S[A] = (bool)B
+                // If C != 0 then PC++
                 OpCode::LoadBool => {
                     stack[inst.A] = LuaValue::Boolean(inst.B > 0).into();
                     if inst.C != 0 {
                         pc += 1;
                     }
                 },
+                // S[A] = E[K[Bx]]
                 OpCode::GetGlobal => {
                     let name = match constants.get(inst.Bx) {
                         Some(n) => n,
@@ -92,6 +116,7 @@ impl VirtualMachine {
                         None => LuaValue::Nil.into()
                     };
                 },
+                // E[K[Bx]] = S[A]
                 OpCode::SetGlobal => {
                     let name = match constants.get(inst.Bx) {
                         Some(n) => n,
@@ -99,12 +124,15 @@ impl VirtualMachine {
                     };
                     self.environment.borrow_mut().insert(name.clone(), stack[inst.A].clone());
                 },
+                // S[A] = UV[B]
                 OpCode::GetUpValue => {
                     stack[inst.A] = upvalues[inst.Bx].clone();
                 },
+                // UV[B] = S[A]
                 OpCode::SetUpValue => {
                     upvalues[inst.B] = stack[inst.A].clone();
                 },
+                // S[A] = S[B][SK[C]]
                 OpCode::GetTable => {
                     let v = match &*stack[inst.B].borrow() {
                         LuaValue::Table(t) => {
@@ -114,6 +142,7 @@ impl VirtualMachine {
                     };
                     stack[inst.A] = v;
                 },
+                // S[A][SK[B]] = SK[C]
                 OpCode::SetTable => {
                     match &mut *stack[inst.A].borrow_mut() {
                         LuaValue::Table(t) => {
@@ -123,6 +152,7 @@ impl VirtualMachine {
                         _ => return LuaResult::Err(LuaError::AttemptedIndexOfNonTable)
                     }
                 },
+                // S[A] = SK[B] <operation> SK[C]
                 OpCode::Add | OpCode::Sub | OpCode::Mul | OpCode::Div | OpCode::Pow | OpCode::Mod => {
                     let lhs = get_rk!(inst.B, constants, stack).borrow().clone();
                     let rhs = get_rk!(inst.C, constants, stack).borrow().clone();
@@ -137,10 +167,12 @@ impl VirtualMachine {
                     };
                     stack[inst.A] = res?.into();
                 },
+                // S[A] = -S[B]
                 OpCode::UnaryMinus => {
                     let v = stack[inst.B].borrow().clone().unm()?;
                     stack[inst.A] = v.into();
                 },
+                // S[A] = not S[B]
                 OpCode::Not => {
                     let v = match *stack[inst.B].borrow() {
                         LuaValue::Boolean(b) => LuaValue::Boolean(!b),
@@ -148,6 +180,7 @@ impl VirtualMachine {
                     };
                     stack[inst.A] = v.into();
                 },
+                // S[A] = length of S[B]
                 OpCode::Len => {
                     let v = match stack[inst.B].borrow().clone() {
                         LuaValue::String(s) => LuaValue::Number((s.len() as f64).into()),
@@ -156,13 +189,16 @@ impl VirtualMachine {
                     };
                     stack[inst.A] = v.into();
                 },
+                // S[A] = concat S[B..C]
                 OpCode::Concat => {
                     let v = stack[inst.B].borrow().clone().concat(stack[inst.C].borrow().clone())?.into();
                     stack[inst.A] = v;
                 },
+                // PC += sBx
                 OpCode::Jmp => {
                     pc += inst.sBx;
                 },
+                // S[A]..S[A+C-1] = S[A](S[A+1]..S[A+B])
                 OpCode::Call => {
                     let mut args = Vec::new();
                     let last_arg_idx = if inst.B == 0 {
@@ -184,6 +220,7 @@ impl VirtualMachine {
                         stack[inst.A + i] = results[i].clone();
                     }
                 },
+                // return S[A]..S[A+B-1]
                 OpCode::Return => {
                     let mut values = Vec::new();
 
@@ -199,6 +236,7 @@ impl VirtualMachine {
 
                     return LuaResult::Ok(values);
                 },
+                // return S[A](S[A+1]..S[A+B-1])
                 OpCode::TailCall => {
                     let mut args = Vec::new();
                     
@@ -208,9 +246,10 @@ impl VirtualMachine {
 
                     return LuaResult::Ok(stack[inst.A].borrow().clone().call(args)?);
                 },
+                // S[A]..S[A+B] = vararg
                 OpCode::Vararg => {
                     let len = if inst.B == 0 {
-                        stack_top = inst.A + vararg.len() - 1;
+                        stack_top = inst.A + vararg.len();
                         vararg.len()
                     } else {
                         inst.B
@@ -224,6 +263,8 @@ impl VirtualMachine {
                         stack[inst.A + i] = v;
                     }
                 },
+                // S[A+1] = S[B]
+                // S[A] = S[B](SK[C])
                 OpCode::LSelf => {
                     stack[inst.A + 1] = stack[inst.B].clone();
                     let v = match &*stack[inst.B].borrow() {
@@ -238,6 +279,7 @@ impl VirtualMachine {
                     };
                     stack[inst.A] = v;
                 },
+                // If SK[B] <operation> SK[C] != A then PC++
                 OpCode::Eq | OpCode::Lt | OpCode::Le => {
                     let lhs = get_rk!(inst.B, constants, stack);
                     let rhs = get_rk!(inst.C, constants, stack);
@@ -271,12 +313,18 @@ impl VirtualMachine {
                         pc += 1;
                     }
                 },
+                // S[A] -= S[A+2]
+                // PC += sBX
                 OpCode::ForPrep => {
-                    let lhs = stack[inst.A].borrow().clone();
-                    let rhs = stack[inst.A + 2].borrow().clone();
-                    stack[inst.A] = lhs.sub(rhs)?.into();
+                    let index = stack[inst.A].borrow().clone();
+                    let step = stack[inst.A + 2].borrow().clone();
+                    stack[inst.A] = index.sub(step)?.into();
                     pc += inst.sBx;
                 },
+                // S[A] += S[A+2]
+                // if S[A] < S[A+1]
+                //   S[A+3] = S[A]
+                //   PC += sBx
                 OpCode::ForLoop => {
                     let index = stack[inst.A].borrow().clone();
                     let limit = stack[inst.A + 1].borrow().clone();
@@ -289,11 +337,17 @@ impl VirtualMachine {
                     };
 
                     if do_loop {
-                        stack[inst.A] = index.clone().into();
-                        stack[inst.A + 3] = index.into();
+                        stack[inst.A] = (index.clone() + step)?.into();
+                        stack[inst.A + 3] = stack[inst.A].clone();
                         pc += inst.sBx;
                     }
                 },
+                // S[A+3]..S[A+2=C] = S[A](S[A+1], S[A+2])
+                // if S[A+3] != nil
+                //   S[A+2] = S[A+3]
+                // else
+                //   PC++
+                // Note: this is entirely untested as I haven't implemented iterator functions yet
                 OpCode::TForLoop => {
                     let results = stack[inst.A].borrow().clone().call(vec![
                         stack[inst.A + 1].clone(),
@@ -311,6 +365,7 @@ impl VirtualMachine {
 
                     pc += 1;
                 },
+                // S[A] = array table of size B, filled with nils
                 OpCode::NewTable => {
                     let mut table: BTreeMap<Rc<RefCell<LuaValue>>, Rc<RefCell<LuaValue>>> = BTreeMap::new();
 
@@ -320,6 +375,7 @@ impl VirtualMachine {
 
                     stack[inst.A] = LuaValue::Table(table).into();
                 },
+                // S[A][(C-1)*FIELDS_PER_FLUSH+i] = S[A+i]
                 OpCode::SetList => {
                     match &mut *stack[inst.A].borrow_mut() {
                         LuaValue::Table(t) => {
@@ -331,11 +387,13 @@ impl VirtualMachine {
                         _ => return LuaResult::Err(LuaError::AttemptedIndexOfNonTable)
                     }
                 },
+                // S[A] = function.prototypes[Bx]
                 OpCode::Closure => {
                     let sub_func = function.prototypes[inst.Bx].clone();
                     let sub_upvalues = if sub_func.upvalue_count > 0 {
                         let mut sub_upvalues: Vec<Rc<RefCell<LuaValue>>> = Vec::new();
 
+                        // Init upvalues
                         for i in 0..sub_func.upvalue_count as usize {
                             let pseudo = &instructions[(pc as usize) + i];
 
@@ -362,6 +420,7 @@ impl VirtualMachine {
                     });
                     stack[inst.A] = LuaValue::Function(func).into();
                 },
+                // UV[0..A] = nil
                 OpCode::Close => {
                     for i in 0..inst.A {
                         upvalues[i] = LuaValue::Nil.into();
